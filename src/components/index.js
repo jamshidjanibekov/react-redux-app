54952ed106297c915dc66f9301c59c0e65af5d7f
jamshid <jamshidjanibekov0@gmail.com>
2023-09-20 11:45:48 +0500

    Navbar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export {default as Main} from './main'
 export {default as Register} from './register'
 export {default as Login} from './login'
+export {default as Navbar} from './navbar'
+
+export class logo {
+}
