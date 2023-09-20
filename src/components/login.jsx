f864788a42c879f2d0deab7b22e50c2c46e5a844
jamshid <jamshidjanibekov0@gmail.com>
2023-09-20 21:00:16 +0500

    Login & Register page & UI

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,25 @@
+import {icon} from "../constans";
+import {Input} from "../ui";
+import {useState} from "react";
+
+
 const Login = () =>{
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
-        <div>Login</div>
+        <div className='text-center'>
+            <main className="form-signin w-25 m-auto">
+                <form>
+                    <img className="mb-5" src={icon} alt="" width="72" height="60"/>
+                    <h1 className="h3 mb-3 fw-normal">Please login</h1>
+
+                    <Input label={'Email address'} state={email} setState={setEmail}/>
+                    <Input label={'Password'} type={'password'} state={password} setState={setPassword}/>
+
+                    <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">Login</button>
+                </form>
+            </main>
+        </div>
     )
 }
 export default Login
