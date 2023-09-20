a5a037566a57b47c3bb7f9636181c05ef950790f
jamshid <jamshidjanibekov0@gmail.com>
2023-09-20 22:03:03 +0500

    Redux. Redux-Toolkit

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,21 @@
 import {icon} from "../constans";
 import {Input} from "../ui";
 import {useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {loginUserStart} from "../slice/auth";
 
 
 const Login = () =>{
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    const {isLoading} = useSelector(state => state.auth)
+
+    const loginHandler = e =>{
+        e.preventDefault()
+        dispatch(loginUserStart())
+    }
+
     return (
         <div className='text-center'>
             <main className="form-signin w-25 m-auto">
@@ -16,7 +26,9 @@ const Login = () =>{
                     <Input label={'Email address'} state={email} setState={setEmail}/>
                     <Input label={'Password'} type={'password'} state={password} setState={setPassword}/>
 
-                    <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">Login</button>
+                    <button className="w-100 btn btn-lg btn-primary mt-2" disabled={isLoading} onClick={loginHandler} type="submit">
+                        {isLoading ? "login..." : 'Login'}
+                    </button>
                 </form>
             </main>
         </div>
