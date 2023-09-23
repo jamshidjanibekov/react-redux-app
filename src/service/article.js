eff69543b36bf1b355f1361468fdf2933699a4a5
jamshid <jamshidjanibekov0@gmail.com>
2023-09-23 07:01:41 +0500

    Article slice. Card

---
+++ src/service/article.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const ArticleService = {
+    async getArticles() {
+        const {data} = await axios.get('/articles')
+        return data
+    }
+}
+export default ArticleService
